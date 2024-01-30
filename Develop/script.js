5b412550cd28af4b5c71244fdbaa1a2d432bd062
Alexsander Rafael <alexsanderrafael2022@gmail.com>
2024-01-30 17:58:04 -0500

    added code to apply class to each time block

--- Develop/script.js
+++ Develop/script.js
@@ -39,6 +39,24 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
+  var currentHour = dayjs().hour();
+
+  // Loop through each time block
+  $(".time-block").each(function() {
+    // Get the hour from the time block's id
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    // Apply the appropriate class based on the comparison
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
